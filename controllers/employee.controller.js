5669a1640c0334df82dc055e1af0f507dac556aa
Nishath Ahamed <117764163+NishathAhamed@users.noreply.github.com>
2023-08-18 18:50:43 +0530

    implemented employee service

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,12 +1,11 @@
 const express =require('express'),router=express.Router()
 
-const db=require('../db')
+const service=require('../services/employee.service')
 
 //http:localhost:3000/api/employees/
 router.get('/',async(req,res)=>{
-  const [rows] =await db.query("SELECT * FROM employee")
-    .catch(err=>console.log(err))
-   res.send(rows)
+  const employees=await service.getAllEmployees()
+   res.send(employees)
 })
 
 module.exports=router;
